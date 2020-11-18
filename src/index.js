73963c4bfed0a3c65aae2540e26005966b6ae17a
ershadul1 <ershadul.rayhan@gmail.com>
2020-11-18 19:35:23 +0600

    Add starter files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles/styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+
+const name = document.querySelector('body');
+name.textContent = "Rayhan";
+name.classList.add('text-danger');
