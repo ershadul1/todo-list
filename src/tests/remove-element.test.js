b21ca4aa20769234730046e1edfdb15a6bcee969
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-12-03 05:48:23 -0300

    Add test for removeCurrentElement

---
+++ src/tests/remove-element.test.js
@@ -0,0 +1,18 @@
+import removeElement from '../modules/helpers/remove-element';
+
+test('Removes element from DOM', () => {
+    const newDiv = document.createElement('div');
+    newDiv.setAttribute('id', 'new-div');
+    
+    const divText = document.createElement('p');
+    divText.textContent += 'Child node';
+
+    newDiv.appendChild(divText);
+    document.body.appendChild(newDiv);
+
+    removeElement('new-div');
+
+    const gettingNewDiv = document.querySelector('#new-div');
+
+    expect(gettingNewDiv.lastElementChild).toBeNull()
+})
